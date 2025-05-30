11b3aaa10c3263b263672cc25aea230f9950e5a5
J-HyeonSu <luvian5028@gmail.com>
2025-05-31 08:41:19 +0900

    feat: ItemData, LevelUpButton
    ScriptableObject ItemData,
    LevelUpButton UI

---
+++ Assets/02. Scripts/Item.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public ItemData data;
+    public int level;
+    public Weapon weapon;
+
+    private Image icon;
+    private Text textLevel;
+
+    private void Awake()
+    {
+        icon = GetComponentsInChildren<Image>()[1];
+        icon.sprite = data.itemIcon;
+
+        Text[] texts = GetComponentsInChildren<Text>();
+        textLevel = texts[0];
+    }
+
+    private void LateUpdate()
+    {
+        textLevel.text = $"Lv. {level + 1}";
+    }
+
+    public void OnClick()
+    {
+        switch (data.itemType)
+        {
+            case ItemData.ItemType.Melee:
+            case ItemData.ItemType.Range:
+                break;
+            case ItemData.ItemType.Glove:
+                break;
+            case ItemData.ItemType.Shoe:
+                break;
+            case ItemData.ItemType.Heal:
+                break;
+        }
+
+        level++;
+        if (level == data.damages.Length)
+        {
+            GetComponent<Button>().interactable = false;
+        }
+    }
+}
+
